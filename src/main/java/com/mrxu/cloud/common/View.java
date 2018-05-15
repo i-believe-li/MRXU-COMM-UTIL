a1a1272ad5ff1affc69aee4b6c73ef6159e8df7c
徐智伟 <xuzhiwei@i-focusing.com>
2018-05-15 16:09:40 +0800

    * #0000: 加入文件服务配置

--- src/main/java/com/mrxu/cloud/common/View.java
+++ src/main/java/com/mrxu/cloud/common/View.java
@@ -1,8 +1,62 @@
 package com.mrxu.cloud.common;
 
+import java.io.Serializable;
+
 /**
- * @author ifocusing-xuzhiwei
- * @since 2018/3/7
+ * @author ifcousing-linfeng
+ * @since 2017/4/18
  */
-public class View {
+public class View<T extends Serializable> {
+
+    public static final Integer OK = 200;
+    public static final Integer ERROR = 5000;
+    
+    private Integer code;
+    private String message;
+    private T data;
+    private String timestamp;
+    private String sign;
+    
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getSign() {
+		return sign;
+	}
+
+	public void setSign(String sign) {
+		this.sign = sign;
+	}
+    
 }
+
