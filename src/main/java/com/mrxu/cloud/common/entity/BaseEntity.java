bfafa5204984ba3c7e99428a94285a93bfcc9f3c
徐智伟 <xuzhiwei@i-focusing.com>
2018-05-15 16:29:04 +0800

    * #0000: 配置修改

---
+++ src/main/java/com/mrxu/cloud/common/entity/BaseEntity.java
@@ -0,0 +1,10 @@
+package com.mrxu.cloud.common.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author ifocusing-xujia
+ * @since 2017/8/25
+ */
+public abstract class BaseEntity implements Serializable{
+}
